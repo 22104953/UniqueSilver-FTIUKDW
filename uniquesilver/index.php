d564bb7803e69754b41bbb6b3be604747eb8758c
22104953 <tisna_komang@yahoo.com>
2013-11-28 22:16:16 +0700

    update all

--- uniquesilver/index.php
+++ uniquesilver/index.php
@@ -1,60 +1,50 @@
 <!DOCTYPE html>
 <head>
-<title>Unique Silver Jewelry</title>
-<link href="image/data/insert%20image/favicon.png" rel="icon" />
-<link rel="stylesheet" type="text/css" href="style/stylesheet/stylesheet.css" />
+	<title>Unique Silver</title>
+	<link rel="stylesheet" type="text/css" href="style/css/stylesheet.css" />
+	<link rel="stylesheet" type="text/css" href="style/css/grid.css" />
 </head>
 <body>
-<div id="container">
-<div id="header">
-<div class="container2">
-  <div class="div1"><marquee><b>SELAMAT DATANG DI WEBSITE KAMI</b></marquee>
-    <div class="div2"><a href="index.php"><img src="image/data/logo/logo.gif" title="Unique Silver Jewelry" alt="Unique Silver Jewelry" /></a></div>
-    <div class="div14"></div>
-    <div class="div3">
-               <a href="keranjang.php" id="tab_cart">KERANJANG<div class="div15"></div></a><a href="akun.php" id="tab_account">AKUN</a><a href="daftar.php" id="tab_daftar">DAFTAR</a>
+	<div class="container_24">
+		<div class="grid_8">
+			<a href="index.php"><img src="image/data/logo/logo.gif" title="Unique Silver Jewelry" alt="Unique Silver Jewelry" /></a>
+		</div>
+		<div class="grid_5 margin_top30 prefix_2">
+			<img src="style/css/image/shipping.png"/>
+		</div>
+		<div class="grid_9 top_menu">
+			<a href="keranjang.php" id="tab_cart">KERANJANG<div class="cart"></div></a>
+			<a href="akun.php" id="tab_account">AKUN</a>
+			<a href="daftar.php" id="tab_daftar">DAFTAR</a>
+    	</div>
+    	<div class="grid_7 prefix_2 search">
+			<b>Cari :</b><input type="text" placeholder="Kata Kunci" id="filter_keyword"/>
+			<a onclick="moduleSearch();" class="button"><span>OK</span></a>
+    	</div>
+    	<div class="clear"></div>
+    	<div class="grid_22 prefix_2 navigasi">
+    		<a href="index.php">BERANDA</a>
+    		<a href="aboutus.php">TENTANG KAMI</a>
+    		<a href="contactus.php">HUBUNGI KAMI</a>
+    		<a href="order.php">INFO ORDER</a>
+    		<a href="shipping.php">INFO PENGIRIMAN</a>
+    	</div>
+    	<div class="clear"></div>
     </div>
-    <div id="search">
-            <div class="div8">Cari : <input type="text" placeholder="Kata Kunci" id="filter_keyword"/></div>
-            <div class="div10"><a onclick="moduleSearch();" class="button" ><span>GO</span></a></div>
-    </div>
-    <div class="div4">
-      <div><a href="index.php">BERANDA</a><a href="aboutus.php">TENTANG KAMI</a><a href="contactus.php">HUBUNGI KAMI</a><a href="order.php">INFO ORDER</a><a class="hide" href="shipping.php">INFO PENGIRIMAN</a></div>
-      </div>
-    </div>
-  </div>
-  <div class="div6">
-    <div id="header-background">
-      <div class="div5">
-        <div class="left"></div>
-        <div class="right"></div>
-      </div>
-    </div>
-    
-      <div id="breadcrumb-wrap">
-        <div class="breadcrumb-wrap-left"><div id="breadcrumb">
-                  </div></div>
-          <div class="breadcrumb-wrap-right"></div>
-        </div>
-       <div class="clear" style="clear:both;"></div>
-      </div>
-    </div>
-  
-</div>
-
-
-<!-- End of Modal Window -->
-<div id="home">
-  <div class="middle">
-   <table width="940px"  border="0" align="center" cellpadding="0" cellspacing="0">
+    <div class="header-background"></div>
+    <div class="clear"></div>
+    <div class="middle">
+    	<div class="container_24">
+    		<div class="grid_24">
+    		 <table>
               <tr>
                 <th scope="col">&nbsp;</th>
                 <th scope="col">&nbsp;</th>
                 <th scope="col">&nbsp;</th>
                 <th scope="col">&nbsp;</th>
               </tr>
-               <tr>
-                <td colspan="2" rowspan="2" align="center"><a href="anting.php"><img src="image/product/home/anting/anting3.jpg" width="463" height="235"/ title="Anting" alt="Anting"></a></td>
+              <tr>
+                <td colspan="2" rowspan="2" align="center"><a href="layout.php"><img src="image/product/home/anting/anting3.jpg" width="463" height="235"/ title="Anting" alt="Anting"></a></td>
                 <td colspan="2" rowspan="2" align="center"><a href="gelang.php"><img src="image/product/home/gelang/gelang1.jpg" width="463" height="235"title="Gelang" alt="Gelang"></a></td>
               </tr>
               <tr>
@@ -77,24 +67,28 @@
                 <td align="center">&nbsp;</td>
                 <td align="center">&nbsp;</td>
               </tr>
-          </table>
-         
-  </div>
-
-</div>
-</div>
+          	</table>
+          </div>
+        </div>
+    </div>
+    <div class="clear"></div>
     <div id="footer">
-      <div class="div3">
+      <div class="sosmed">
         <b>Follow Us :</b>
-        <a href="https://twitter.com/uniquesilverj"><img src="style/image/twitter.png"/></a>
-        <a href="https://www.facebook.com/unique.silver.5?fref=ts"><img src="style/image/facebook.png"/></a>
+        <a href="https://twitter.com/uniquesilverj"><img src="style/css/image/twitter.png"/></a>
+        <a href="https://www.facebook.com/unique.silver.5?fref=ts"><img src="style/css/image/facebook.png"/></a>
       </div>
-      <div class="div4">
+      <div class="div1">
         <a href="faq.php">FAQ</a>
         <a href="map.php">SITE MAP</a>
         <a href="syarat.php">SYARAT &amp; KETENTUAN</a>
       </div>
+  </div>
     </div>
-    <div class="div2" style="background-color:#000000; margin-top: -2px;padding-top: 1px;padding-left: 5px;padding-bottom: 10px;">
-    Copyright &copy; 2013 <a href="index.php" target="_blank" alt="Unique Silver Jewelry" title="Unique Silver Jewelry">Unique Silver Jewelry</a><br /></div>
+    <div class="copy_right">
+    Copyright &copy; 2013 <a href="index.php" target="_blank" alt="Unique Silver Jewelry" title="Unique Silver Jewelry">Unique Silver Jewelry</a><br />
+</body>
 </html> 
+
+
+
---
+++ uniquesilver/layout.php
@@ -0,0 +1,316 @@
+<!DOCTYPE html>
+<head>
+	<title>Unique Silver</title>
+	<link rel="stylesheet" type="text/css" href="style/css/stylesheet.css" />
+	<link rel="stylesheet" type="text/css" href="style/css/grid.css" />
+</head>
+<body>
+	<div class="container_24">
+		<div class="grid_8">
+			<a href="index.php"><img src="image/data/logo/logo.gif" title="Unique Silver Jewelry" alt="Unique Silver Jewelry" /></a>
+		</div>
+		<div class="grid_5 margin_top30 prefix_2">
+			<img src="style/css/image/shipping.png"/>
+		</div>
+		<div class="grid_9 top_menu">
+			<a href="keranjang.php" id="tab_cart">KERANJANG<div class="cart"></div></a>
+			<a href="akun.php" id="tab_account">AKUN</a>
+			<a href="daftar.php" id="tab_daftar">DAFTAR</a>
+    	</div>
+    	<div class="grid_7 prefix_2 search">
+			<b>Cari :</b><input type="text" placeholder="Kata Kunci" id="filter_keyword"/>
+			<a onclick="moduleSearch();" class="button"><span>OK</span></a>
+    	</div>
+    	<div class="clear"></div>
+    	<div class="grid_22 prefix_2 navigasi">
+    		<a href="index.php">BERANDA</a>
+    		<a href="aboutus.php">TENTANG KAMI</a>
+    		<a href="contactus.php">HUBUNGI KAMI</a>
+    		<a href="order.php">INFO ORDER</a>
+    		<a href="shipping.php">INFO PENGIRIMAN</a>
+    	</div>
+    	<div class="clear"></div>
+    </div>
+    <div class="header-background"></div>
+    <div class="clear"></div>
+    <div class="container_24 margin_top30">
+    	<div id="column_left">
+    		<table class="tabel_box">
+    			<tr>
+    				<td>
+    					<table cellpadding="3" cellspacing="0" style="width: 100%;">
+	    					<tr>
+	    						<td align="center" colspan="2" class="top"><b>PRODUK</b></td>
+	    					</tr>
+	          				<tr>
+				            	<td valign="top" width="1"><a href="gelang.php"><img src="image/data/chace/gl.jpg" alt="Gelang" /></a></td>
+				            	<td align="left"><a href="gelang.php">Gelang</a>
+	          				</tr>
+				         	<tr>
+				            	<td valign="top" width="1"><a href="anting.php"><img src="image/data/chace/ant.jpg" alt="Anting" /></a></td>
+				            	<td align="left"><a href="anting.php">Anting</a>
+				         	</tr>
+			          		<tr>
+			            		<td valign="top" width="1"><a href="kalung.php"><img src="image/data/chace/kl.jpg" alt="Kalung" /></a></td>
+			            		<td align="left"><a href="kalung.php">Kalung</a>
+			          		</tr>
+			          		<tr>
+				            	<td valign="top" width="1"><a href="cincin.php"><img src="image/data/chace/cn.jpg" alt="cincin" /></a></td>
+				            	<td align="left"><a href="cincin.php">Cincin</a>
+			          		</tr>
+	          			</table>
+	          		</td>
+	          	</tr>
+	        </table>
+	        <table class="tabel_box">
+    			<tr>
+    				<td>
+    					<table cellpadding="3" cellspacing="0" style="width: 100%;">
+	    					<tr>
+	    						<td align="center" class="top"><b>INFORMASI</b></td>
+	    					</tr>
+	    					<tr>
+	    						<td>
+						          <ul>
+						            <li><a href="order.php">Cara Berbelanja</a></li>
+						            <li><a href="syarat.php">Syarat &amp; Ketentuan</a></li>
+						            <li><a href="faq.php">FAQ</a></li>
+						            <li><a href="contactus.php">Hubungi Kami</a></li>
+						            <li><a href="map.php">Peta Situs</a></li>
+						          </ul>
+      							</td>
+      						</tr>
+	          			</table>
+	          		</td>
+	          	</tr>
+	        </table>
+	        <table class="tabel_box">
+	        	<tr>
+    				<td>
+    					<table cellpadding="3" cellspacing="0" style="width: 100%;">
+    						<tr>
+	    						<td colspan="2" align="center" class="top"><b>Customer Service</b></td>
+	    					</tr>
+	    					<tr>
+	    						<td>						          
+						            No Handphone :
+						            +6281391192520
+						        </td>
+						    </tr>
+						    <tr>
+						        <td>
+						            (Monday- Sunday)</br>
+						            (9 am - 9 pm wib)				            						         
+      							</td>
+      						</tr>
+	          			</table>
+	          		</td>
+	          	</tr>
+	        </table>
+	    </div>
+		<div id="column_right">
+				<table class="tabel_box">
+    			<tr>
+    				<td>
+    					<table cellpadding="3" cellspacing="0" style="width: 100%;">
+	    					<tr>
+	    						<td align="center" colspan="2" class="top"><b><img src="style/css/image/cart.png"/>Keranjang</b></td>
+	    					</tr>
+	          			</table>
+	          		</td>
+	          	</tr>
+	        </table>
+	        <table class="tabel_box">
+    			<tr>
+    				<td>
+    					<table cellpadding="3" cellspacing="0" style="width: 100%;">
+	    					<tr>
+	    						<td colspan="2" align="center" class="top"><b>PRODUK BARU</b></td>
+	    					</tr>
+	    					<tr>
+              					<td valign="top" width="1"><a href="an0001.php"><img src="image/data/chace/AN0001.jpg"/></a></td>
+              					<td valign="center"><a href="an0001.php"><span style="font-size: 11px;">Rectanglular Pave</span></a>
+                    			<br />
+                    			<span style="font-size: 11px; color: #900;">Rp.160,000.00</span>
+                				</td>
+            				</tr>
+            				<tr>
+              					<td valign="top" width="1"><a href="cn0001.php"><img src="image/data/chace/CN0001.jpg"/></a></td>
+              					<td valign="center"><a href="cn0001.php"><span style="font-size: 11px;">Sterling Silver Huggie</span></a>
+			                    <br />
+			                    <span style="font-size: 11px; color: #900;">Rp.155,000.00</span>
+              					</td>
+					            </tr>
+					            <tr>
+					              <td valign="top" width="1"><a href="gl0001.php"><img src="image/data/chace/GL0001.jpg"/></a></td>
+					              <td valign="center"><a href="gl0001.php"><span style="font-size: 11px;">Rope Link Bracelet</span></a>
+					              <br />
+					               <span style="font-size: 11px; color: #900;">Rp.135,000.00</span>
+					              </td>
+					            </tr>
+					            <tr>
+					              <td valign="top" width="1"><a href="cn0002.php"><img src="image/data/chace/CN0002.jpg"/></a></td>
+					              <td valign="center"><a href="cn20001.php"><span style="font-size: 11px;">Cluster Cocktail Ring</span></a>
+					              <br />
+					              <span style="font-size: 11px; color: #900;">Rp.172,000.00</span>
+					              </td>
+					            </tr>
+					            <tr>
+					              <td valign="top" width="1"><a href="cn0003.php"><img src="image/data/chace/CN0003.jpg"/></a></td>
+					              <td valign="center"><a href="cn20003.php"><span style="font-size: 11px;">Natural Amethyst Ring</span></a>
+					              <br />
+					              <span style="font-size: 11px; color: #900;">Rp.153,000.00</span>
+					              </td>
+					            </tr>
+					            <tr>
+					              <td valign="top" width="1"><a href="cn0004.php"><img src="image/data/chace/CN0004.jpg"/></a></td>
+					              <td valign="center"><a href="cn20004.php"><span style="font-size: 11px;"> Blue Topaz Ring</span></a>
+					              <br />
+					              <span style="font-size: 11px; color: #900;">Rp.189,000.00</span>
+					              </td>
+					            </tr>
+					            <tr>
+					              <td valign="top" width="1"><a href="cn0005.php"><img src="image/data/chace/CN0005.jpg"/></a></td>
+					              <td valign="center"><a href="cn20005.php"><span style="font-size: 11px;"> Silver Filigree Ring</span></a>
+					              <br />
+					              <span style="font-size: 11px; color: #900;">Rp.389,000.00</span>
+					              </td>
+					            </tr>
+	          			</table>
+	          		</td>
+	          	</tr>
+	        </table>
+	        <table class="tabel_box">
+	        	<tr>
+    				<td>
+    					<table cellpadding="2" cellspacing="0" style="width: 100%;">
+    					 	<tr>
+	    						<td colspan="2" align="center" class="top"><b>Favorit</b></td>
+	    					</tr>
+				            <tr>
+				              <td valign="top" width="1"><a href="kl0002.php"><img src="image/data/chace/KL0002.jpg"/></a></td>
+				              <td valign="center"><a href="kl0002.php"><span style="font-size: 11px;">Light of Om</span></a>
+				                    <br />
+				                    <span style="font-size: 11px; color: #900;">Rp.260,000.00</span>
+				                </td>
+				              </tr>
+				            <tr>
+				              <td valign="top" width="1"><a href="cn0006.php"><img src="image/data/chace/CN0006.jpg"/></a></td>
+				              <td valign="center"><a href="cn0006.php"><span style="font-size: 11px;">Filigree Solitaire Ring</span></a>
+				                    <br />
+				                    <span style="font-size: 11px; color: #900;">Rp.205,000.00</span>
+				              </td>
+				            </tr>
+				        </table>
+	          		</td>
+	          	</tr>
+	        </table>
+	    </div>
+	    </div>
+	</div>
+	  <div id="content">
+      <div class="top">
+        <div class="left"></div>
+        <div class="right"></div>
+        <div class="center">
+          <h1>ANTING</h1>
+          </div>
+      </div>
+      <div class="produk_tengah">
+        <div class="produk_">
+          <a href="an0001.php"><img src="image/product/anting/AN0001-120x120.jpg" title="Rectanglular Pave" alt="Rectanglular Pave" /><br/>
+          <a href="an0001.php">Rectanglular Pave </a><br/>
+          <span style="color: #999; font-size: 11px;">AN0001</span><br/>
+          <span style="color: #900; font-weight: bold;">Rp.160,000.00</span><br/>
+        </div>
+        <div class="produk_">
+          <a href="an0002.php"><img src="image/product/anting/AN0002-120x120.jpg" title="Silver Dangle Earrings" alt="Silver Dangle Earrings" /></a><br/>
+          <a href="an0002.php">Silver Dangle Earrings</a><br/>
+          <span style="color: #999; font-size: 11px;">AN0002</span><br/>
+          <span style="color: #900; font-weight: bold;">Rp.200,000.00</span><br/>
+        </div>
+        <div class="produk_">
+          <a href="an0014.php"><img src="image/product/anting/AN0014-120x120.jpg" title="Crystal Glass" alt="Crystal Glass" /></a><br />
+          <a href="an0014.php">Crystal Glass</a><br />
+          <span style="color: #999; font-size: 11px;">AN0014</span><br />
+          <span style="color: #900; font-weight: bold;">Rp.200,000.00</span><br />
+        </div>
+        <div class="produk_">
+         <a href="an0004.php"><img src="image/product/anting/AN0004-120x120.jpg" title="Silver Dangle Earrings" alt="Silver Dangle Earrings" /></a><br />
+          <a href="an0004.php">Silver Dangle Earrings</a><br />
+          <span style="color: #999; font-size: 11px;">AN0004</span><br />
+          <span style="color: #900; font-weight: bold;">Rp.180,000.00</span><br />
+        </div>
+        <div class="produk_">
+          <a href="an0005.php"><img src="image/product/anting/AN0005-120x120.jpg" title="Ornate 925 Sterling Silver" alt="Ornate 925 Sterling Silver" /></a><br />
+          <a href="an0005.php">Ornate 925 Sterling Silver</a><br />
+          <span style="color: #999; font-size: 11px;">AN0005</span><br />
+          <span style="color: #900; font-weight: bold;">Rp.190,000.00</span><br />
+        </div>
+         <div class="produk_">
+          <a href="an0011.php"><img src="image/product/anting/AN0011-120x120.jpg" title="Lovely 925 Sterling Silver" alt="Lovely 925 Sterling Silver" /></a><br />
+          <a href="an0011.php">Lovely 925 Sterling Silver</a><br />
+          <span style="color: #999; font-size: 11px;">AN0011</span><br />
+          <span style="color: #900; font-weight: bold;">Rp.210,000.00</span><br />
+        </div>        
+        <div class="produk_">
+          <a href="an0007.php"><img src="image/product/anting/AN0007-120x120.jpg" title="Lovely 925 Sterling Silver" alt="Lovely 925 Sterling Silver" /></a><br />
+          <a href="an0007.php">Lovely 925 Sterling Silver</a><br />
+          <span style="color: #999; font-size: 11px;">AN0002</span><br />
+          <span style="color: #900; font-weight: bold;">Rp.185,000.00</span><br />
+        </div>
+        <div class="produk_">
+          <a href="an0013.php"><img src="image/product/anting/AN0013-120x120.jpg" title="Ornate 925 Sterling Silver" alt="Ornate 925 Sterling Silver" /></a><br />
+          <a href="an0005.php">Ornate 925 Sterling Silver</a><br />
+          <span style="color: #999; font-size: 11px;">AN0013</span><br />
+          <span style="color: #900; font-weight: bold;">Rp.190,000.00</span><br />
+        </div>
+        <div class="produk_">
+          <a href="an0010.php"><img src="image/product/anting/AN0010-120x120.jpg" title="Crystal Glass" alt="Crystal Glass" /></a><br />
+          <a href="an0010.php">Crystal Glass</a><br />
+          <span style="color: #999; font-size: 11px;">AN0010</span><br />
+          <span style="color: #900; font-weight: bold;">Rp.190,000.00</span><br />
+        </div>
+       <div class="produk_">
+          <a href="an0006.php"><img src="image/product/anting/AN0006-120x120.jpg" title="Crystal Glass" alt="Crystal Glass" /></a><br />
+          <a href="an0006.php">Crystal Glass</a><br />
+          <span style="color: #999; font-size: 11px;">AN0006</span><br />
+          <span style="color: #900; font-weight: bold;">Rp.200,000.00</span><br />
+        </div>
+        <div class="produk_">
+          <a href="an0012.php"><img src="image/product/anting/AN0012-120x120.jpg" title="Silver Dangle Earrings" alt="Silver Dangle Earrings" /></a><br />
+          <a href="an0012.php">Silver Dangle Earrings</a><br />
+          <span style="color: #999; font-size: 11px;">AN0012</span><br />
+          <span style="color: #900; font-weight: bold;">Rp.210,000.00</span><br />               
+        </div>
+        <div class="produk_">
+          <a href="an0009.php"><img src="image/product/anting/AN0009-120x120.jpg" title="Swarovski Crystal" alt="Swarovski Crystal" /></a><br />
+          <a href="an0009.php">Swarovski Crystal</a><br />
+          <span style="color: #999; font-size: 11px;">AN0009</span><br />
+          <span style="color: #900; font-weight: bold;">Rp.250,000.00</span><br />                 
+        </div>
+        <div class="produk_">
+          <a href="an0003.php"><img src="image/product/anting/AN0003-120x120.jpg" title="Lovely 925 Sterling Silver" alt="Lovely 925 Sterling Silver" /></a><br />
+          <a href="an0003.php">Lovely 925 Sterling Silver</a><br />
+          <span style="color: #999; font-size: 11px;">AN0002</span><br />
+          <span style="color: #900; font-weight: bold;">Rp.166,000.00</span><br />
+        </div>
+      </div>
+    </div>
+	<div id="footer">
+      <div class="sosmed">
+        <b>Follow Us :</b>
+        <a href="https://twitter.com/uniquesilverj"><img src="style/css/image/twitter.png"/></a>
+        <a href="https://www.facebook.com/unique.silver.5?fref=ts"><img src="style/css/image/facebook.png"/></a>
+      </div>
+      <div class="div1">
+        <a href="faq.php">FAQ</a>
+        <a href="map.php">SITE MAP</a>
+        <a href="syarat.php">SYARAT &amp; KETENTUAN</a>
+      </div>
+  </div>
+    </div>
+    <div class="copy_right">
+    Copyright &copy; 2013 <a href="index.php" target="_blank" alt="Unique Silver Jewelry" title="Unique Silver Jewelry">Unique Silver Jewelry</a><br />
+</body>
+</html> 
